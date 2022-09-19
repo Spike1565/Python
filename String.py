8d7ccbf4ce582993b22571f2a159e0f745e57744
satyasure <51406383+satyasure@users.noreply.github.com>
2022-09-19 21:20:49 +0800

    add String.py

---
+++ String.py
@@ -0,0 +1,35 @@
+#1
+phrase_1 = 'My name is Lalith'
+phrase_2 = 'Adithya Sure'
+phrase_3 = phrase_1 + ' ' + phrase_2
+
+#to add space between 2 phrase use phrase_3 = phrase_2 + ' ' + phrase_3
+#without space just use phrase_3 = phrase_1 + phrase_2
+
+#2
+input ('Who are you?')
+#the input asks the user something that you enter 
+
+#add space to give space after the question mark for the answer
+input ('Who are you? ')
+
+#for new line add /n before after the space
+input ('Who are you? \n')
+
+#to give indication to the user add >>> to make it easy for the user to write
+input ('Who are you?\n>>>')
+
+#to save it use variable
+user_input = input ('Who are you?\n >>>')
+print (user_input)
+
+#type is used to know the type of output
+#use int (input ('Who are you?')) to change the output to an integer
+int (input ('Who are you?'))
+#an error appears if u add anything except a number
+
+#3
+#use help() to open Python Help Utility
+help ()
+#to quit help use quit in the terminal
+#You are not allowed to use Python Keywords as variable names
