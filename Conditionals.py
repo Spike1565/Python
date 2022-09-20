5cb825a47a06fd1508516263c2e9587432030e67
Spike1565 <87360679+Spike1565@users.noreply.github.com>
2022-09-20 18:48:57 +0800

    Add files via upload

---
+++ Conditionals.py
@@ -0,0 +1,20 @@
+#BOOLEAN EXPRESSION
+#x = 5
+#y = 6
+#print ('x =',x, 'y =',y,)
+#print ('Checking less than with \'>\':',x < y,)
+#print ('Checking greater than with \'>\':' ,x > y,)
+
+from typing import Type
+
+
+var_1 = 7
+var_2 = 7
+print ('var_1 =',var_1,'var_2',var_2,)
+print ('Checking equality with \'==\':',var_1 == var_2,)
+print ('Checking not equality with \'!=\':',var_1 != var_2,)
+print ('Checking less than with \'<=\':',var_1 <= var_2,)
+print ('Checking greater than with \'>=\':',var_1 >= var_2,)
+
+print ('Demonstrate that \'True\' is Boolean in python with \
+    \'type(True)\':',type(True))
